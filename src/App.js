651354476a95dab08ab67f0791415b8e7edaea2f
Femi <femzy123@yahoo.com>
2020-12-22 18:45:16 +0100

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src="https://cdn.dribbble.com/users/1161517/screenshots/7896076/media/24ae74ddb6c9eb7789ae3a189a6b30ae.gif" className="App-logo" alt="logo" />
       </header>
+      <div>
+        <h2>Just a simple app to test out Amplify</h2>
+      </div>
     </div>
   );
 }
